997511be94e487746c37d90cacaaf9d43f3bf235
Suresh Patidar <suresh.patidar@gmail.com>
2023-01-23 06:43:14 +0000

    Update 2 files

--- src/index.js
+++ src/index.js
@@ -9,25 +9,29 @@ import "./styles.css";
 const data = {
   labels: [
     "Leads",
-    "Conversaron con un asesor",
-    "Visitaron una propiedad",
-    "Compraron",
-    "Conversion"
+    "Cold calls and Email Campain",
+    "Sales Call",
+    "Engineering Deep Dive Discussion",
+    "Proposals",
+    "Project Win",
   ],
   subLabels: ["Web", "Call Center", "Facebook", "Google"],
   colors: [
     ["#FFB178", "#FF78B1", "#FF3C8E"],
     ["#A0BBFF", "#EC77FF"],
     ["#A0F9FF", "#7795FF"],
-    ["#A0F9FF", "#7795FF"]
+    ["#A0F9FF", "#7795FF"],
+    ["#A0F9FF", "#7795FF"],
+    ["#A0F9FF", "#7795FF"],
   ],
   values: [
-    [2000, 1451, 5000, 20],
-    [2500, 1400, 1023, 30],
-    [400, 200, 119, 40],
-    [100, 40, 22, 50],
-    [100, 40, 22, 50]
-  ]
+    [2000, 1000, 700, 300],
+    [1500, 700, 300, 200],
+    [1200, 400, 230, 178],
+    [900, 158, 120, 97],
+    [300, 108, 73, 49],
+    [120, 70, 35, 15],
+  ],
 };
 
 class App extends Component {
@@ -40,7 +44,7 @@ class App extends Component {
       direction: "horizontal",
       width: 800,
       height: 350,
-      subLabelValue: "percent"
+      subLabelValue: "percent",
     });
 
     graph.draw();
